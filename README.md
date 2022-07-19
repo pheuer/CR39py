9a5da332f8c499cafd971c1408937628ed33d53e
Peter Heuer <pvheuer@gmail.com>
2022-07-19 14:04:03 -0400

    Initial package structure

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CR39py
- Code for processing CR39 particle track data
+Code for analyzing CR39 particle track data
+
+# Installation Instructions
+If you intend to use this package without changing the code, follow the instructions to install as a user. If you plan on making changes to the code, install in developer mode. 
+
+## As User
+
+Run the 
+`pip install git+https://github.com/pheuer/CR39py.git`
+
+## Development Mode
+1. Clone the repository
+2. Navigate to the repository directory and run the command `python -m pip install -e .`
+
+
+## Confirming successful installation
+Try running the command 
+`import cr39py`
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='CR39py',
+    version='0.0.1',
+    author='Peter Heuer',
+    author_email='pheu@lle.rochester.edu',
+    description='Code for analyzing CR39 particle track data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/pheuer/CR39py',
+    project_urls = {
+        "Bug Tracker": "https://github.com/pheuer/CR39py/issues"
+    },
+    license='MIT',
+    packages=['cr39py'],
+    install_requires=['numpy', 'h5py', 'matplotlib',],
+)
